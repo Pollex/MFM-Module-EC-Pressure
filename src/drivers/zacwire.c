ff344c78c93f9e1b2380ac50eb92e04d9261a8fd
Tim van Osch <timvanosch@pollex.nl>
2024-02-23 15:04:35 +0100

    Implement ZACWire with duty cycle calibration

--- src/drivers/zacwire.c
+++ src/drivers/zacwire.c
@@ -1,52 +1,66 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <util/atomic.h>
 
 #include "board/mfm_sensor_module.h"
 #include "drivers/zacwire.h"
 
-#define IDLE_COUNTS_MS                                                         \
-    (F_CPU /                                                                   \
-     1000) // F_CPU is counts per second, div by 1000 to get counts per ms
-#define IDLE_COUNTS IDLE_COUNTS_MS / 2 // 0.5ms
-
-volatile enum State {
-    STATE_WAIT_FOR_IDLE,
-    STATE_WAIT_FOR_START_BIT,
-    STATE_WAIT_FOR_FIRST_BIT,
-    STATE_WAIT_FOR_BIT,
-    STATE_BIT_SAMPLING,
-    STATE_DONE
-} state;
-
-volatile struct {
-    uint8_t duty_cycle;
-    uint8_t bit_count;
-    uint8_t *data_ptr;
-    uint8_t read_count;
-    uint8_t parity;
-} state_ctx;
-
-void timer_set(uint16_t counts) {
-    TCB0.CNT = 0;
-    TCB0.CCMP = counts;
-    TCB0.CTRLA = TCB_ENABLE_bm;
-}
+#define IDLE_COUNTS 150 // 272us
 
 /// @brief Initialize the zacwire bus
 void zacwire_init() {
     // Set the pin as input
-    ZACWIRE_PORT.DIR &= ~ZACWIRE_PIN;
-    //
-    // Prepare timer
-    //
-    // TCB uses CLK_PER which is based on F_CPU so 3_333_333Hz in this case
-    // One tick would be around 300ns
-    // No clk div is used
-    // TCB0.CTRLA = 0;
-    // Set to single shot mode, and capture events when counter reaches TOP
-    // require an interrupt, only the counted value
-    TCB0.CTRLB = TCB_CNTMODE_SINGLE_gc;
+    ZACWIRE_PORT.DIRCLR = ZACWIRE_PIN;
+}
+
+inline void wait_for_idle(void) {
+    // Wait for bus idle
+    uint16_t idle = IDLE_COUNTS;
+    while (idle) {
+        if (ZACWIRE_PORT.IN & ZACWIRE_PIN)
+            idle--;
+        else
+            idle = IDLE_COUNTS;
+    }
+}
+
+inline void wait_till_low(void) {
+    while (!(ZACWIRE_PORT.IN & ZACWIRE_PIN))
+        ;
+    while (ZACWIRE_PORT.IN & ZACWIRE_PIN)
+        ;
+}
+
+inline void wait_till_duty(uint8_t cycles) {
+    while (cycles--)
+        __asm("nop");
+}
+
+void zacwire_read_byte(uint8_t *data, uint8_t *parity) {
+    uint8_t duty = 0;
+    uint8_t bits = 8;
+
+    // Measure half duty using start bit (not a data bit!)
+    wait_till_low();
+    while (!(ZACWIRE_PORT.IN & ZACWIRE_PIN))
+        duty++;
+
+    while (bits--) {
+        wait_till_low();
+        wait_till_duty(duty);
+        PORTA.OUTTGL = PIN1_bm;
+        // Sample
+        *data <<= 1;
+        if ((ZACWIRE_PORT.IN & ZACWIRE_PIN) > 0) {
+            *data |= 1;
+            *parity += 1;
+        }
+    }
+    wait_till_low();
+    wait_till_duty(duty);
+    if ((ZACWIRE_PORT.IN & ZACWIRE_PIN) > 0)
+        *parity += 1;
 }
 
 /// @brief Read a byte from the bus
@@ -55,111 +69,13 @@ void zacwire_init() {
 /// @return validity of the data: 0 is valid, -1 is invalid
 /// @condition The bus is idle
 int8_t zacwire_read(uint8_t *data, uint8_t count) {
-    // Reset SM and enable interrupts
-    state = STATE_WAIT_FOR_IDLE;
-    state_ctx.parity = 0;
-    state_ctx.bit_count = 0;
-    state_ctx.duty_cycle = 0;
-    state_ctx.data_ptr = data;
-    state_ctx.read_count = count;
-    ZACWIRE_PORT.PIN6CTRL |= PORT_ISC_FALLING_gc;
-    TCB0.INTCTRL = TCB_CAPT_bm;
-    // Start idle timer, this will interrupt the timer after 0.5 ms, or
-    // if a falling edge is detected, this timer will be reset
-    timer_set(IDLE_COUNTS);
-
-    // Wait for SM to finish
-    while (state != STATE_DONE)
-        ;
-
-    // Disable interrupts
-    ZACWIRE_PORT.PIN6CTRL &= ~PORT_ISC_FALLING_gc;
-    TCB0.INTCTRL = 0;
-
-    if (state_ctx.parity % 2) {
-        return -1;
-    }
-    return 0;
-}
-
-void sm_on_falling_edge(void) {
-    switch (state) {
-    case STATE_WAIT_FOR_IDLE:
-        // We reached a low edge, meaning a transaction is busy. Reset the
-        // timer.
-        timer_set(IDLE_COUNTS);
-        break;
-    case STATE_WAIT_FOR_START_BIT:
-        // A falling edge while waiting for the start bit. start counter
-        timer_set(0xffff);
-        // At the next falling edge, store the counts
-        state = STATE_WAIT_FOR_FIRST_BIT;
-        break;
-    case STATE_WAIT_FOR_FIRST_BIT:
-        state_ctx.duty_cycle = TCB0.CNT /2;
-        // fallthrough to bit start
-    case STATE_WAIT_FOR_BIT:
-        // Set the timer to the duty cycle sample point.
-        timer_set(state_ctx.duty_cycle);
-        // After the timer interrupts, it will sample the bit and either
-        // move to BYTE_END or back to this state
-        state = STATE_BIT_SAMPLING;
-        break;
-
-    // These states should not have falling edges
-    case STATE_BIT_SAMPLING:
-    case STATE_DONE:
-        break;
-    }
-}
-
-void sm_on_timer(void) {
-    switch (state) {
-    case STATE_WAIT_FOR_IDLE:
-        // The timer is only triggered in this state if no falling edge is
-        // detected during 1ms
-        state = STATE_WAIT_FOR_START_BIT;
-        break;
-    case STATE_BIT_SAMPLING: {
-        // Timer triggers at 1/2 Cycle so we should sample NOW!
-        uint8_t sample = ((ZACWIRE_PORT.IN & ZACWIRE_PIN) > 0);
-        state_ctx.parity += sample;
-        state_ctx.bit_count++; // Increment first, so "1-based" counter
-        // These are still data bits
-        if (state_ctx.bit_count < 9) {
-            *state_ctx.data_ptr <<= 1;
-            *state_ctx.data_ptr |= sample;
-            state = STATE_WAIT_FOR_BIT;
-        } else {
-            // We're at the 9th bit (parity bit)
-            // If we require more bytes to be read, loop back to wait for start bit;
-            if ((--state_ctx.read_count) > 0) {
-                state_ctx.data_ptr++;
-                state = STATE_WAIT_FOR_START_BIT;
-            } else {
-                // Otherwise, goto IDLE
-                state = STATE_DONE;
-            }
+    uint8_t parity;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        wait_for_idle();
+        while (count--) {
+            zacwire_read_byte(data, &parity);
+            data++;
         }
-        break;
     }
-
-    // Timer should not trigger in these states!
-    case STATE_WAIT_FOR_START_BIT:
-    case STATE_WAIT_FOR_FIRST_BIT:
-    case STATE_WAIT_FOR_BIT:
-    case STATE_DONE:
-        break;
-    }
-}
-
-ISR(PORTA_PORT_vect) {
-    PORTA.OUTTGL = PIN1_bm;
-    //sm_on_falling_edge();
-    ZACWIRE_PORT.INTFLAGS = ZACWIRE_PIN;
-}
-
-ISR(TCB0_INT_vect) {
-    sm_on_timer();
-    TCB0.INTFLAGS = 1;
+    return -(parity % 2);
 }
